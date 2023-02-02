03e728f1cb8f66e1324a3982cd505785e3064be4
ROdrigoRRRRD <roddrigo2003cod@gmail.com>
2023-02-02 17:01:41 -0600

    tres

--- index.php
+++ index.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
     <title>Calculadora</title>
-    <link rel="stylesheet" href="./Styles.css">
+    <link rel='stylesheet' href='./Style.css'>
 </head>
 <body>
-    <section class="grid-Container">
+    <section class='grid-Container'>
         <div class='tittle_Name'>
-            <h1>Conversor numerico</h1>
+            <h1>Conversor</h1>
+            <h1>Numérico</h1>
         </div>
         <div class='form-Container'>
-            <form method="post">
-                <select name="select1">
-                    <option value="decimal">Decimal</option>
-                    <option value="binario">Binario</option>
-                    <option value="hexadecimal">Hexadecimal</option>
-                    <option value="octal">Octal</option>
-                </select>
-                <input type="text" name="numeroi" placeholder="Ingresa el numero a convertir">
-                <label for="">A que sistema deseas convertir?</label>
-                <select name="select2">
-                    <option value="decimal">Decimal</option>
-                    <option value="binario">Binario</option>
-                    <option value="hexadecimal">Hexadecimal</option>
-                    <option value="octal">Octal</option>
-                </select>
-                <input type="submit" name="convertir">
-            </form>
-            <?php
-                include('registrar.php');
-            ?>
+            <div class='form-Section'>
+                <form method='post'>
+                    <select name='select1'>
+                        <option value='decimal'>Decimal</option>
+                        <option value='binario'>Binario</option>
+                        <option value='hexadecimal'>Hexadecimal</option>
+                        <option value='octal'>Octal</option>
+                    </select>
+                    <input type='text' name='numeroi' placeholder='Ingresa el numero a convertir'>
+                    <label for=''>¿A que sistema deseas convertir?</label>
+                    <select name='select2'>
+                        <option value='decimal'>Decimal</option>
+                        <option value='binario'>Binario</option>
+                        <option value='hexadecimal'>Hexadecimal</option>
+                        <option value='octal'>Octal</option>
+                    </select>
+                    <input type='submit' name='convertir'>
+                </form>
+            </div>
+            <div class='reply-Section'>
+                <h1>Resultado</h1>
+                <?php
+                    include('registrar.php');
+                ?>
+            </div>
         </div>
     </section>
 </body>
